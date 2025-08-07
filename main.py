f4fe96793367f92f4d4fa6d41cb7db0deff7acff
Dmitry <dmitry.sostin@yandex.ru>
2025-08-08 00:08:23 +0300

    Первый день подсобрал точки графов в json файле и немного наабросал первый экран

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 """  Main file algoritm  """
 import pygame
+import json
 
 
 fps = 30                                            #  количество кадров в секунду
+H = 1080
+W = 700
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
 
 
 def main():
     """Start function"""
+    with open("points.json", "r") as f:
+        points = json.load(f)["points"]               # Графы
+    with open("points.json", "r") as f:
+        orders = json.load(f)["orders"]               # Параметры груза
+    with open("points.json", "r") as f:
+        vehicles = json.load(f)["vehicles"]           # Параметры транспортного средства
+
+
     pygame.init()
     clock = pygame.time.Clock()
-    pygame.display.set_mode((1080,700))
+    screen = pygame.display.set_mode((H, W))
     pygame.display.set_caption("Planet Express")
     pygame.display.set_icon(pygame.image.load("statick\icon.png"))
 
+    screen.fill(WHITE)
+    font = pygame.font.SysFont('couriernew', 10)
+    #text = font.render("Hello", True, (255, 0, 0))
+    #screen.blit(text, (50, 50))
 
+    for i in range(16):
+        pygame.draw.circle(screen, GREEN, (points[i]["x"], points[i]["y"]), 15, width=3)
+        text = font.render(points[i]['name'], True, BLUE)
+        screen.blit(text, (points[i]["x"] + 25, points[i]["y"] -5))
+        print(points[i]['name'], points[i]["x"], points[i]["y"])
     
 
     run = True
@@ -22,23 +48,16 @@ def main():
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 run = False
-            if e.type == pygame.KEYDOWN:
-                my_key = e.key
-                my_mod = e.mod
-                print(f"Нажата клавиша: {pygame.key.name(my_key)}")
-                print(f"Модификатор: {my_mod}")
-            if e.type == pygame.MOUSEBUTTONDOWN:
-                my_pos = e.pos
-                my_button = e.button
-                print(f"Позиция мыши: {my_pos}")
-                print(f"Идентификатор кнопки мыши: {my_button}")
-
+        
+        pygame.display.flip()
 
     """2-й Раздел ----- Логика игры ------------------"""
 
 
 
     """# 3-й Раздел -----  Отображение графики ---------"""
+
+
     clock.tick(fps)
     pygame.quit()
 
